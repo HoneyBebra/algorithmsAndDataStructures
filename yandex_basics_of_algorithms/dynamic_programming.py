98f46ae9e217b8c0fff6cfede7b34691bea5a782
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2024-01-05 01:13:22 +1000

    Added rocks 2

--- yandex_basics_of_algorithms/dynamic_programming.py
+++ yandex_basics_of_algorithms/dynamic_programming.py
@@ -9,3 +9,17 @@ def rocks(n_m: str) -> str:
         return 'Loose'
     else:
         return 'Win'
+
+
+def rocks_2(n_m: str) -> str:
+    # https://new.contest.yandex.ru/48558/problem?id=215/2023_04_06/HFqqJtCCRh
+
+    n_m = n_m.split()
+    n = int(n_m[0])
+    m = int(n_m[1])
+
+    zero_coordinate = max(n, m) - min(n, m)
+    if zero_coordinate % 3 == 0:
+        return 'Loose'
+    else:
+        return 'Win'
